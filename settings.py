fdae8104dc4570ce686c8b6b350f344272d145a2
Apostolos Chalis <tolishalis@gmail.com>
2026-09-08 10:47:22 +0300

    Resolve rights issue

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "data"
 FEEDS_FILE = DATA_DIR / "feeds.json"
 
-APP_DATA_DIR = BASE_DIR / ".local_data"
+APP_DATA_DIR = BASE_DIR / ".local" / "share" / "uninews"
 APP_DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 DATABASE_PATH = APP_DATA_DIR / "uninews.db"
